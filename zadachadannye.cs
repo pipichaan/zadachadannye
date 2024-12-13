76fbfffa1c8352b0a2aad7a22d7e50b1b329cdb4
Алина Широкова <147584671+pipichaan@users.noreply.github.com>
2024-12-14 00:34:37 +0300

    Add files via upload

---
+++ zadachadannye.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+class MyProgram
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string inputText = File.ReadAllText("C:\\Users\\alina\\OneDrive\\Рабочий стол\\2 курс\\Zadacha_dannye.txt");
+            int maximumPairs = MAx(inputText);
+            Console.WriteLine("макс. пар =  " + maximumPairs);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    static int MAx(string inputText)
+    {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int index = 0; index < inputText.Length - 1; index++)
+        {
+            string cur = inputText.Substring(index, 2);
+            if (cur == "AB" || cur == "CB")
+            {
+                currentCount++;
+                index++;
+            }
+            else
+            {
+                maxCount = Math.Max(maxCount, currentCount);
+                currentCount = 0;
+            }
+        }
+
+        maxCount = Math.Max(maxCount, currentCount);
+        return maxCount;
+    }
+}
